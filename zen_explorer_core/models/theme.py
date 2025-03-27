c15710d2e7722c82f724f3dca473acd29b241a58
green <41323182+greeeen-dev@users.noreply.github.com>
2025-03-27 20:19:16 +0100

    Fix repo check

---
+++ zen_explorer_core/models/theme.py
@@ -0,0 +1,85 @@
+from enum import Enum
+from datetime import datetime
+
+class ThemeType(Enum):
+    bundle = 0
+    chrome = 1
+    content = 2
+
+class Theme:
+    def __init__(self, data: dict, install_data: dict):
+        self._raw_data: dict = data
+        self._raw_install_data: dict = install_data
+
+    @property
+    def name(self) -> str:
+        return self._raw_data.get('name')
+
+    @property
+    def author(self) -> str:
+        return self._raw_data.get('author')
+
+    @property
+    def type(self) -> ThemeType:
+        return ThemeType(self._raw_data.get('type'))
+
+    @property
+    def type_name(self) -> str:
+        if self.type == ThemeType.bundle:
+            return 'bundle'
+        elif self.type == ThemeType.chrome:
+            return 'theme'
+        elif self.type == ThemeType.content:
+            return 'page'
+        else:
+            return 'unknown'
+
+    @property
+    def description(self) -> str:
+        return self._raw_data.get('description')
+
+    @property
+    def author_url(self) -> str:
+        return self._raw_data.get('authorUrl')
+
+    @property
+    def homepage(self) -> str:
+        return self._raw_data.get('homepage')
+
+    @property
+    def version(self) -> str:
+        return self._raw_data.get('version')
+
+    @property
+    def created_at(self) -> datetime:
+        # convert unix time to datetime
+        return datetime.fromtimestamp(self._raw_data.get('createdAt'))
+
+    @property
+    def updated_at(self) -> datetime:
+        # convert unix time to datetime
+        return datetime.fromtimestamp(self._raw_data.get('updatedAt'))
+
+    @property
+    def tags(self) -> list:
+        return self._raw_data.get('tags')
+
+    @property
+    def files(self) -> list:
+        return self._raw_install_data.get('files')
+
+    @property
+    def folders(self) -> list:
+        return self._raw_install_data.get('folders')
+
+    @property
+    def chrome_targets(self) -> list:
+        return self._raw_install_data.get('uclChromeTarget')
+
+    @property
+    def content_targets(self) -> list:
+        return self._raw_install_data.get('uclContentTarget')
+
+    @property
+    def raw_install_data(self) -> dict:
+        return self._raw_install_data
